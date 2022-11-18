094b039776332753525ba6ecb1902a7f17a7f438
osvaldo <iosvaldo.app@gmail.com>
2022-11-18 00:34:29 -0500

    update the right side of the header 💰

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './Header.css'
 import { BsSearch } from "react-icons/bs";
 import logo from './images/linkedin.png';
+import HeaderOption from './HeaderOption';
+import {AiFillHome} from "react-icons/ai"
+import { BsPeopleFill } from "react-icons/bs";
+import { BsFillBriefcaseFill } from "react-icons/bs";
+import { AiFillMessage } from "react-icons/ai";
+import { MdNotificationsActive } from "react-icons/md";
+import avatar from "./images/man.png";
 
 function Header() {
   return (
@@ -9,10 +16,7 @@ function Header() {
       {/* <h1>This is the Header</h1> */}
 
       <div className="header__left">
-        <img
-          src={logo}
-          alt="linkedin"
-        />
+        <img src={logo} alt="linkedin" />
 
         <div className="header__search">
           <BsSearch />
@@ -20,11 +24,16 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
-
-
+      <div className="header__right">
+        <HeaderOption Icon={AiFillHome} title="Home" />
+        <HeaderOption Icon={BsPeopleFill} title="My Network" />
+        <HeaderOption Icon={BsFillBriefcaseFill} title="Jobs" />
+        <HeaderOption Icon={AiFillMessage} title="Messages" />
+        <HeaderOption Icon={MdNotificationsActive} title="Notification" />
+        <HeaderOption avatar={avatar} title="Me" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
